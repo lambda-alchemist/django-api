12286278b320b85a952927b3b019f9793327271f
Guilherme M. Miranda <alchemist.software@proton.me>
2022-12-05 23:25:44 -0300

    changes

--- my_api/urls.py
+++ my_api/urls.py
@@ -8,11 +8,11 @@ from drf_yasg import openapi
 schema_view = get_schema_view(
 	openapi.Info(
 	title="Snippets API",
-    	default_version='v1',
-    	description="Test description",
-    	terms_of_service="https://www.google.com/policies/terms/",
-    	contact=openapi.Contact(email="contact@snippets.local"),
-    	license=openapi.License(name="BSD License"),
+		default_version='v1',
+		description="Test description",
+		terms_of_service="https://www.google.com/policies/terms/",
+		contact=openapi.Contact(email="contact@snippets.local"),
+		license=openapi.License(name="BSD License"),
 	),
 	public=True,
 	permission_classes=[permissions.AllowAny],
@@ -22,10 +22,10 @@ router = routers.DefaultRouter()
 router.register(r'api/v1/users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+	path('', include(router.urls)),
+	path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+	path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 	path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 	re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 	re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
