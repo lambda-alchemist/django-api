b5667020794e5ef578249e55f973d02cf81d52e1
Guilherme M. Miranda <alchemist.software@proton.me>
2022-12-22 15:15:00 -0300

    changes

--- my_api/urls.py
+++ my_api/urls.py
@@ -1,6 +1,5 @@
-import django
 from django.urls import path, re_path, include
-from django.contrib import admin, admindocs
+from django.contrib import admin
 from rest_framework import routers, permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
@@ -26,22 +25,22 @@ schema_view = get_schema_view(
 )
 
 router = routers.DefaultRouter()
-router.register(r'api/v1/users', UserViewSet)
-router.register(r'api/v1/products', ProductViewSet)
-router.register(r'api/v1/category', CategoryViewSet)
-router.register(r'api/v1/address', AddressViewSet)
-router.register(r'api/v1/purchase', PurchaseViewSet)
-router.register(r'api/v1/item', CartItemViewSet)
-router.register(r'api/v1/cart', CartViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'category', CategoryViewSet)
+router.register(r'addr', AddressViewSet)
+router.register(r'receit', PurchaseViewSet)
+router.register(r'item', CartItemViewSet)
+router.register(r'cart', CartViewSet)
 
 
 urlpatterns = [
 	path('', include(router.urls)),
 	path('admin/', admin.site.urls),
 	path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-	path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-	path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-	path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+	path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+	path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 	re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 	re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 	re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
